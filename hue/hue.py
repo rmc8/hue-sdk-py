e9650f4e84cda617a51254e3246fbfcc755ff692
mikohei <mikohei@amazon.co.jp>
2021-09-19 16:05:16 +0900

    [ADD] Method to convert a response to a DataFrame

--- hue/hue.py
+++ hue/hue.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import requests
 from retry import retry
+from pandas.io.json import json_normalize
 
 from . import util
 from .all import All
@@ -25,7 +26,7 @@ class Hue:
         ip: str = settings["ip"]
         self.user_name: str = settings["user_name"]
         self.base: str = f"http://{ip}/api/{{user_name}}"
-        self.get = All(self)
+        self.all = All(self)
     
     @retry(
         delay=util.AUTH_FAILURE_SLEEP,
@@ -46,3 +47,15 @@ class Hue:
         logger.debug(f"{method}: {endpoint}")
         logger.debug(res)
         return res
+    
+    @staticmethod
+    def to_dataframe(res: dict, id_exists=False):
+        if id_exists:
+            table = []
+            for key, value in res.items():
+                value["id"] = key
+                table.append(value)
+            df = json_normalize(table, max_level=5)
+            columns = [col for col in df.columns.tolist() if col != "id"]
+            return df[["id"] + columns[:-1]]
+        return json_normalize(list(res.values()), max_level=5)
